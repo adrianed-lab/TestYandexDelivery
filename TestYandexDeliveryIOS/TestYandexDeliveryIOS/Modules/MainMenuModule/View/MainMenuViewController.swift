ce19d27e63c3d5715a4a1cb8e6ecf641b8cc551d
Roman Gushchik <adrianed.fabled@gmail.com>
2022-10-15 23:56:46 +0300

    update MainMenuViewController

--- TestYandexDeliveryIOS/TestYandexDeliveryIOS/Modules/MainMenuModule/View/MainMenuViewController.swift
+++ TestYandexDeliveryIOS/TestYandexDeliveryIOS/Modules/MainMenuModule/View/MainMenuViewController.swift
@@ -9,22 +9,31 @@ import UIKit
 
 protocol MainMenuViewProtocol: AnyObject {
     func successGetProducts()
+    func successGetCategories()
     func failure(error: Error)
 }
 
 class MainMenuViewController: UIViewController, MainMenuViewProtocol {
-    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var categoriesCollectionView: UICollectionView!
+    @IBOutlet weak var menuTableView: UITableView!
     var presenter: MainMenuViewPresenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        menuTableView.register(UINib(nibName: "MainMenuTableViewCell", bundle: nil), forCellReuseIdentifier: MainMenuTableViewCell.key)
+        categoriesCollectionView.register(UINib(nibName: "CategoriesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: CategoriesCollectionViewCell.key)
     }
     
     func successGetProducts() {
-        
+        menuTableView.reloadData()
     }
     
+    func successGetCategories() {
+        categoriesCollectionView.reloadData()
+    }
+
     func failure(error: Error) {
         print(error.localizedDescription)
     }
